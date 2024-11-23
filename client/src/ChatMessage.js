8b6c33eae62d166461acc4fe86527f33727dfa90
nathanstrauss13 <nathanstrauss@gmail.com>
2024-11-23 09:06:06 -0500

    Update ChatMessage.js

--- client/src/ChatMessage.js
+++ client/src/ChatMessage.js
@@ -1,1 +1,12 @@
+import React from 'react';
+
+const ChatMessage = ({ sender, text }) => {
+  return (
+    <div className={sender}>
+      <strong>{sender === 'user' ? 'You' : 'AI'}:</strong> {text}
+    </div>
+  );
+};
+
+export default ChatMessage;
 
